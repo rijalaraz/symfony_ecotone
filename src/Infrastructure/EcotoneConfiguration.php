e0030cb7f3b13658c774ea8b3dfd5ed36b40e3e5
Rija Lalaina <rijalaraz@gmail.com>
2026-03-11 22:21:19 +0300

    Configure Dbal

--- src/Infrastructure/EcotoneConfiguration.php
+++ src/Infrastructure/EcotoneConfiguration.php
@@ -2,6 +2,7 @@
 
 namespace App\Infrastructure;
 
+use Ecotone\Dbal\Configuration\DbalConfiguration;
 use Ecotone\Messaging\Attribute\ServiceContext;
 use Ecotone\SymfonyBundle\Config\SymfonyConnectionReference;
 
@@ -10,6 +11,12 @@ final readonly class EcotoneConfiguration
     #[ServiceContext()]
     public function dbalConfiguration()
     {
-        return SymfonyConnectionReference::defaultConnection('default');
+        // return SymfonyConnectionReference::defaultConnection('doctrine');
+        return DbalConfiguration::createWithDefaults()
+            ->withDoctrineORMRepositories(
+                true,
+                //[Article::class]
+            )
+        ;
     }
 }
