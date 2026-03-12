268f8c393850a2808a9fb32868cadf849d2cd4eb
Rija Lalaina <rijalaraz@gmail.com>
2026-03-12 07:44:19 +0300

    EventSourcingAggregate

--- src/Domain/Product/Product.php
+++ src/Domain/Product/Product.php
@@ -6,18 +6,22 @@ use App\Domain\Product\Command\CreateProduct;
 use App\Domain\Product\Event\ProductWasCreated;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\Table;
-use Ecotone\Modelling\Attribute\Aggregate;
 use Ecotone\Modelling\Attribute\AggregateIdentifier;
 use Ecotone\Modelling\Attribute\CommandHandler;
 use Ecotone\Modelling\Attribute\Distributed;
+use Ecotone\Modelling\Attribute\EventSourcingAggregate;
+use Ecotone\Modelling\Attribute\EventSourcingHandler;
+use Ecotone\Modelling\WithAggregateVersioning;
 use Ramsey\Uuid\Uuid;
 
 #[ORM\Entity]
 #[Table("products")]
-#[Aggregate]
+#[EventSourcingAggregate]
 class Product
 {
     const CREATE_PRODUCT_PRODUCT = "product.createProduct";
+
+    use WithAggregateVersioning;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -57,5 +61,13 @@ class Product
     public function getPrice(): int
     {
         return $this->price;
+    }
+
+    #[EventSourcingHandler]
+    public function applyProductWasCreated(ProductWasCreated $event): void
+    {
+        $this->productId = $event->productId;
+        $this->name = $event->name;
+        $this->price  = $event->price;
     }
 }
